48acb46a44359c2899fc3ca38f7680110658989f
Oliver Rausch <oliverrausch99@gmail.com>
2020-09-08 12:49:40 +0200

    Fix torch module

---
+++ daceml/onnx/shape_inference/shape_inference.py
@@ -0,0 +1,10 @@
+from daceml.onnx.shape_inference.symbolic_shape_infer import SymbolicShapeInference
+
+class ShapeInferenceError(ValueError):
+    pass
+
+def infer_shapes(onnx_path, out_path):
+    try:
+        SymbolicShapeInference.infer_shapes(onnx_path, out_path)
+    except SystemExit:
+        raise ShapeInferenceError("Unable to infer shapes for ONNX model.")
--- daceml/pytorch/module.py
+++ daceml/pytorch/module.py
@@ -1,39 +1,55 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import onnx
-import numpy as np
+try:
+    import torch
+except ModuleNotFoundError:
+    raise ImportError("To use the torch frontend, please install torch")
+
+import os
+import tempfile
 from functools import wraps
 
-import dace
-from dace.frontend.onnx import ONNXModel
-from .symbolic_shape_infer import SymbolicShapeInference
+import torch.nn as nn
+import onnx
+
+from daceml.onnx import ONNXModel
+from daceml.onnx.shape_inference import infer_shapes
+
 
 class DACEModule(nn.Module):
     def __init__(self, model, dummy_inputs=None):
         super(DACEModule, self).__init__()
-        
+
         self.model = model
         self.sdfg = None
         if dummy_inputs is not None:
-            self.initialize_sdfg(dummy_inputs)
+            self.dace_model = self.initialize_sdfg(dummy_inputs)
 
-    def initialize_sdfg(self, dummy_inputs):
-        input_names = [ "actual_input_1" ]
-        torch.onnx.export(self.model, dummy_inputs, "model.onnx", verbose=True, input_names=input_names, opset_version=12)
-        SymbolicShapeInference.infer_shapes("model.onnx", "shape_infer.onnx")
+    def initialize_sdfg(self, dummy_inputs) -> ONNXModel:
+        export_fd, export_name = tempfile.mkstemp(suffix=".onnx")
+        infer_fd, infer_name = tempfile.mkstemp(suffix=".onnx")
 
-        onnx_model = onnx.load("./shape_infer.onnx")
-        self.dace_model = ONNXModel("dace_model", onnx_model)
-        self.sdfg = self.dace_model.sdfg
+        torch.onnx.export(
+            self.model,
+            dummy_inputs,
+            export_name,
+            verbose=True,  # TODO read log level
+            opset_version=12)
+
+        infer_shapes(export_name, infer_name)
+
+        onnx_model = onnx.load(infer_name)
+        dace_model = ONNXModel("dace_model", onnx_model)
+        self.sdfg = dace_model.sdfg
         self.sdfg.validate()
-        self.sdfg.save("./model.sdfg")
+
+        os.close(export_fd)
+        os.close(infer_fd)
+        return dace_model
 
     def forward(self, *actual_inputs):
         if self.sdfg is None:
-      	    self.initialize_sdfg(actual_inputs)
+            self.dace_model = self.initialize_sdfg(actual_inputs)
 
-        return self.dace_model(*actual_inputs) 
+        return self.dace_model(*(inp.numpy() for inp in actual_inputs))
 
 
 def module(moduleclass):
@@ -44,4 +60,5 @@ def module(moduleclass):
     @wraps(moduleclass)
     def _create(*args, **kwargs):
         return DACEModule(moduleclass(*args, **kwargs))
+
     return _create
