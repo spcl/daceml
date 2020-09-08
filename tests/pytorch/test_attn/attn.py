3e93774a3f972782f4103237cc892e9670d4f6e1
Oliver Rausch <oliverrausch99@gmail.com>
2020-09-08 12:49:51 +0200

    Fix attn test

--- tests/pytorch/test_attn/attn.py
+++ tests/pytorch/test_attn/attn.py
@@ -1,49 +1,32 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import onnx
 import numpy as np
-import onnxruntime as ort
 
-import dace
-from dace.frontend.onnx import ONNXModel
-from dace.frontend.pytorch import DACEModule
+from daceml.pytorch import DACEModule
 
 
-B = 2
-H = 16
-P = 64
-N = P*H
-SM, SN = 512, 512
-#K, Q, V = (torch.randn([SM, B, N], requires_grad=True).cuda(),
-#           torch.randn([SN, B, N], requires_grad=True).cuda(),
-#           torch.randn([SM, B, N], requires_grad=True).cuda())
-K, Q, V = (torch.randn([SM, B, N]),
-           torch.randn([SN, B, N]),
-           torch.randn([SM, B, N]))
+def test_attn(gpu):
+    B = 2
+    H = 16
+    P = 64
+    N = P * H
+    SM, SN = 512, 512
+    K, Q, V = [
+        torch.randn([SM, B, N]),
+        torch.randn([SN, B, N]),
+        torch.randn([SM, B, N])
+    ]
+    if gpu:
+        K, Q, V = K.cuda(), Q.cuda(), V.cuda()
 
-ptmodel = torch.nn.MultiheadAttention(N, H, bias=False)
+    ptmodel = torch.nn.MultiheadAttention(N, H, bias=False)
+    pt_outputs = ptmodel(Q, K, V)
 
-############################## Torch ################################
-pt_outputs = ptmodel(Q, K, V)
-print(pt_outputs[0])
+    dace_model = DACEModule(ptmodel)
+    dace_outputs = dace_model(Q, K, V)
 
-############################ DaCe #################################
-my_dace_model = DACEModule(ptmodel)
-#my_dace_model = DACEModule(ptmodel, (Q, K, V))
-
-dace_outputs = my_dace_model(Q, K, V) 
-#dace_outputs = my_dace_model(Q.numpy(), K.numpy(), V.numpy()) 
-print(dace_outputs[0])
-
-############################## Ort #################################
-sess = ort.InferenceSession("shape_infer.onnx")
-ort_outputs = sess.run(None, {"actual_input_1": Q.numpy(), "key": K.numpy(), "value": V.numpy()})
-print(ort_outputs[0])
-
-
-
-assert np.allclose(dace_outputs[0], ort_outputs[0])
-assert np.allclose(dace_outputs[1], ort_outputs[1])
-assert np.allclose(pt_outputs[0].detach().numpy(), ort_outputs[0], rtol=8e-02)
-assert np.allclose(pt_outputs[1].detach().numpy(), ort_outputs[1], rtol=8e-02)
+    assert np.allclose(pt_outputs[0].detach().numpy(),
+                       dace_outputs[0],
+                       atol=1e-07)
+    assert np.allclose(pt_outputs[1].detach().numpy(),
+                       dace_outputs[1],
+                       atol=1e-07)
