d170bd112ad10c06e2457aab49a3e4d8a013e0bc
Oliver Rausch <oliverrausch99@gmail.com>
2020-09-14 22:54:49 +0200

    Add more asserts to constant folding test

--- tests/transformation/test_constant_folding.py
+++ tests/transformation/test_constant_folding.py
@@ -14,10 +14,13 @@ def test_bert_subgraph():
     dace_model = donnx.ONNXModel("reshape", model)
 
     out_before = dace_model()
+    assert len(dace_model.sdfg.nodes()[0].nodes()) > 2
 
     dace_model.sdfg.apply_transformations_repeated(
         [ConstantFolding, RedundantSecondArray], validate_all=True)
 
     out_after = dace_model()
 
+    # assert that only two nodes remain
+    assert len(dace_model.sdfg.nodes()[0].nodes()) == 2
     assert np.allclose(out_before, out_after)
