fa49f25b9813576e844d86cba17e8706bc253244
Shigang Li <shigangli.cs@gmail.com>
2020-11-06 17:47:46 +0100

    test cf for attn

--- tests/pytorch/test_attn.py
+++ tests/pytorch/test_attn.py
@@ -3,6 +3,8 @@ import numpy as np
 
 from daceml.pytorch import DaceModule
 
+from dace.transformation.dataflow import RedundantSecondArray
+from daceml.transformation import ConstantFolding
 
 def test_attn(gpu):
     B = 2
@@ -20,13 +22,17 @@ def test_attn(gpu):
     pt_outputs = ptmodel(Q, K, V)
 
     dace_model = DaceModule(ptmodel, cuda=gpu)
-    dace_outputs = dace_model(Q, K, V)
+    dace_outputs_0 = dace_model(Q, K, V)
+
+    dace_model.dace_model.sdfg.apply_transformations_repeated(
+        [ConstantFolding, RedundantSecondArray], validate_all=True)
+    dace_outputs_1 = dace_model(Q, K, V)
 
     assert np.allclose(pt_outputs[0].detach().numpy(),
-                       dace_outputs[0],
+                       dace_outputs_1[0],
                        atol=1e-06)
     assert np.allclose(pt_outputs[1].detach().numpy(),
-                       dace_outputs[1],
+                       dace_outputs_1[1],
                        atol=1e-06)
     print("testing passed")
 
