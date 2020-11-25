4a9b61386db178394569c57d21f6f5cf075e1b4d
Shigang Li <shigangli.cs@gmail.com>
2020-11-25 22:01:38 +0100

    trans testing passed on cpu

--- tests/pytorch/test_bert_encoder_trans.py
+++ tests/pytorch/test_bert_encoder_trans.py
@@ -23,11 +23,16 @@ def test_bert_cf(gpu):
 
     dace_model = DaceModule(ptmodel, cuda=gpu, train=False)
     dace_outputs0 = dace_model(input.clone())
+    dace_model.dace_model.sdfg.save("before_trans.sdfg")
 
     dace_model.dace_model.sdfg.apply_transformations_repeated(
         [ConstantFolding, RedundantSecondArray], validate_all=True)
+    dace_model.dace_model.sdfg.apply_transformations_repeated(
+        [ReshapeTransposeElimination], validate_all=True)
+    dace_model.dace_model.sdfg.save("after_trans_before_expand.sdfg")
     dace_model.dace_model.sdfg.expand_library_nodes()
     dace_model.dace_model.sdfg.apply_strict_transformations()
+    dace_model.dace_model.sdfg.save("after_expand.sdfg")
 
     dace_outputs1 = dace_model(input.clone())
 
@@ -35,16 +40,8 @@ def test_bert_cf(gpu):
 
     assert np.max(diff) < 1e-5
     assert np.allclose(dace_outputs1, dace_outputs0, rtol=1e-05)
-    print("CF testing passed")
+    print("Transformations (including CF and RTE) testing passed")
 
-    dace_model.dace_model.sdfg.apply_transformations_repeated(
-        [ReshapeTransposeElimination], validate_all=True)
-    dace_model.dace_model.sdfg.expand_library_nodes()
-    dace_model.dace_model.sdfg.apply_strict_transformations()
-    #dace_model.dace_model.sdfg.save("delreshape_trans.sdfg")
-    dace_outputs2 = dace_model(input.clone())
-    assert np.allclose(dace_outputs2, dace_outputs1, rtol=1e-05)
-    print("ReshapeTransElimination testing passed")
 
 if __name__ == "__main__":
     test_bert_cf(False)
