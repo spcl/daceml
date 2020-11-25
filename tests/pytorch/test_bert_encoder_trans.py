c24281bcc79bf3e33aa5e005e76c3f9ce90aefde
Shigang Li <shigangli.cs@gmail.com>
2020-11-25 15:20:00 +0100

    ReshapeTransEli test

---
+++ tests/pytorch/test_bert_encoder_trans.py
@@ -0,0 +1,51 @@
+import pytest
+import numpy as np
+import torch
+from dace.transformation.dataflow import RedundantSecondArray
+from transformers import BertConfig, BertLayer
+
+import daceml.onnx as donnx
+from daceml.pytorch import DaceModule
+from daceml.transformation import ConstantFolding, ReshapeTransposeElimination
+donnx.default_implementation = "pure"
+
+
+@pytest.mark.ort
+def test_bert_cf(gpu):
+    batch_size = 8
+    seq_len = 512
+    hidden_size = 768
+
+    input = torch.randn([batch_size, seq_len, hidden_size])
+
+    ptmodel = BertLayer(BertConfig()).eval()
+    pt_outputs = ptmodel(input.clone())
+
+    dace_model = DaceModule(ptmodel, cuda=gpu, train=False)
+    dace_outputs0 = dace_model(input.clone())
+
+    dace_model.dace_model.sdfg.apply_transformations_repeated(
+        [ConstantFolding, RedundantSecondArray], validate_all=True)
+    dace_model.dace_model.sdfg.expand_library_nodes()
+    dace_model.dace_model.sdfg.apply_strict_transformations()
+
+    dace_outputs1 = dace_model(input.clone())
+
+    diff = np.abs(dace_outputs0 - pt_outputs[0].detach().numpy())
+
+    assert np.max(diff) < 1e-5
+    assert np.allclose(dace_outputs1, dace_outputs0, rtol=1e-05)
+    print("CF testing passed")
+
+    dace_model.dace_model.sdfg.apply_transformations_repeated(
+        [ReshapeTransposeElimination], validate_all=True)
+    dace_model.dace_model.sdfg.expand_library_nodes()
+    dace_model.dace_model.sdfg.apply_strict_transformations()
+    #dace_model.dace_model.sdfg.save("delreshape_trans.sdfg")
+    dace_outputs2 = dace_model(input.clone())
+    assert np.allclose(dace_outputs2, dace_outputs1, rtol=1e-05)
+    print("ReshapeTransElimination testing passed")
+
+if __name__ == "__main__":
+    test_bert_cf(False)
+    #test_bert_cf(True)
