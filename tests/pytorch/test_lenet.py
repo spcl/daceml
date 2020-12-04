ca14593d50261d391b466f86c0eeec5c647bb295
Oliver Rausch <oliverrausch99@gmail.com>
2020-12-04 11:03:26 +0100

    Move image ops to own file

--- tests/pytorch/test_lenet.py
+++ tests/pytorch/test_lenet.py
@@ -11,8 +11,8 @@ import torch.nn.functional as F
 class LeNet(nn.Module):
     def __init__(self):
         super(LeNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, 3)
-        self.conv2 = nn.Conv2d(6, 16, 3)
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 6 * 6, 120)  # 6*6 from image dimension
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
