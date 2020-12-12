b5c372682846a88b7595e294cb91e774b55d0f37
Tiziano De Matteis <tiziano.dematteis@inf.ethz.ch>
2020-12-12 19:50:35 +0100

    Convert access nodes to vectorized type for conv

--- tests/pytorch/test_im2col_conv2d_fpga.py
+++ tests/pytorch/test_im2col_conv2d_fpga.py
@@ -15,11 +15,13 @@ import numpy as np
 import daceml.onnx as donnx
 from daceml.pytorch import DaceModule, dace_module
 import copy
+import dace
+from daceml.util import utils
 
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.conv = nn.Conv2d(6, 16, 5)
+        self.conv = nn.Conv2d(1, 6, 5)
 
         self.conv.weight = torch.nn.Parameter(torch.ones_like(self.conv.weight))
         # self.conv = nn.Conv2d(4, 4, 3)
@@ -35,34 +37,40 @@ donnx.default_implementation = "pure"
 donnx.ONNXConv.default_implementation = 'im2col'
 
 ptmodel = Model()
+data_shape = (100,1,28,28)
+vec_width = 4
 
-# numpy_array = np.arange(0, 1*2*4*4, dtype=np.float32).reshape(1,2,4,4)
-# x = torch.from_numpy(numpy_array)
-x = torch.rand(100, 6, 24, 24)
-# x = torch.ones(1, 1, 4, 4)
+x = torch.rand(data_shape)
 
 dace_model = DaceModule(ptmodel)
 dace_output = dace_model(x)
 
 torch_output = ptmodel(x)
-# dace_model.sdfg.expand_library_nodes()
 dace_model.sdfg.save('/tmp/out.sdfg')
 
 assert np.allclose(torch_output.detach().numpy(), dace_output, atol=1e-06)
 
-
-# Transform to FPGA
-#
+# Save sdfg to file
 sdfg = dace_model.sdfg
 orig_sdfg = copy.deepcopy(sdfg)
 orig_sdfg.expand_library_nodes()
 orig_sdfg.save('/tmp/out_expanded.sdfg')
-#
-donnx.ONNXConv.default_implementation = "fpga"
+
+##################################
+# Vectorize input and output container
+
+vec_type = dace.vector(dace.float32, vec_width)
+utils.vectorize_array_and_memlet(sdfg, "ONNX_input", vec_type)
+utils.vectorize_array_and_memlet(sdfg, "ONNX_3", vec_type)
+
+##################################
+# Transfor to FPGA
+
 sdfg.apply_transformations([FPGATransformSDFG])
 sdfg.states()[0].location["is_FPGA_kernel"]=False
 sdfg.states()[0].nodes()[0].sdfg.states()[0].location["is_FPGA_kernel"]=False
 sdfg.save('/tmp/out_fpga.sdfg')
+donnx.ONNXConv.default_implementation = "fpga"
 
 sdfg.expand_library_nodes()
 sdfg.save('/tmp/out_fpga_expanded.sdfg')
