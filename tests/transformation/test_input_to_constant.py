6a9d563ebb9adcf1e9aa51f175c129e926ceb45c
Tiziano De Matteis <tiziano.dematteis@inf.ethz.ch>
2020-12-14 13:02:25 +0100

    Test input to constat, add FPGA

--- tests/transformation/test_input_to_constant.py
+++ tests/transformation/test_input_to_constant.py
@@ -4,8 +4,11 @@ import torch.nn as nn
 
 import dace
 import daceml.onnx as donnx
+import copy
 from daceml.pytorch import DaceModule
 from daceml.transformation import InputToConstant
+from dace.transformation.interstate import FPGATransformSDFG, InlineSDFG
+
 
 
 class TestModule(nn.Module):
@@ -24,13 +27,28 @@ def test_input_to_constant():
     dace_net = DaceModule(net, dummy_inputs=(torch.rand(10, 5), ))
 
     inp = torch.rand((10, 5))
+
+    fpga_dace_net = copy.deepcopy(dace_net)
     #
     sdfg: dace.SDFG = dace_net.sdfg
+
     sdfg.expand_library_nodes()
     sdfg.apply_transformations_repeated([InputToConstant], print_report=True)
-    sdfg.view()
 
     torch_result = net(torch.clone(inp))
     dace_result = dace_net(torch.clone(inp))
-
     assert np.allclose(torch_result.detach().numpy(), dace_result)
+    donnx.ONNXGemm.default_implementation = "fpga"
+    sdfg.save('/tmp/out.sdfg')
+    sdfg = fpga_dace_net.sdfg
+    sdfg.apply_transformations([FPGATransformSDFG])
+
+    sdfg.expand_library_nodes()
+    sdfg.apply_transformations_repeated([InputToConstant], print_report=True)
+    sdfg.save('/tmp/out_fpga.sdfg')
+    dace_output_fpga = fpga_dace_net(torch.clone(inp))
+    assert np.allclose(torch_result.detach().numpy(), dace_output_fpga)
+
+
+
+test_input_to_constant()
