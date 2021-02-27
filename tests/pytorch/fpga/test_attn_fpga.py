d376e9c20794a1b41c7e484c30ff1bd08f57b942
Tiziano De Matteis <tdematt@inf.ethz.ch>
2021-02-27 10:58:29 +0100

    MHA fpga use onnxruntime expansion for Cast

--- tests/pytorch/fpga/test_attn_fpga.py
+++ tests/pytorch/fpga/test_attn_fpga.py
@@ -11,7 +11,7 @@ donnx.default_implementation = "pure"
 from dace.transformation.interstate import FPGATransformSDFG, InlineSDFG
 from dace.transformation.dataflow import PruneConnectors
 from dace.transformation.dataflow import streaming_memory as sm
-
+from dace import StorageType
 from dace import SDFG
 
 @pytest.mark.ort
@@ -20,11 +20,11 @@ def test_attn(execute_cpu_dace = False):
     # BERT_large: H=16, P=64, N=1024, emb=4N, SM=SN=512
 
     ##### Tiny BERT
-    # B = 2
-    # H = 4
-    # P = 8
-    # N = P * H
-    # SM, SN = 16, 16
+    B = 2
+    H = 4
+    P = 8
+    N = P * H
+    SM, SN = 16, 16
 
     ##### SMALL BERT
     # B = 2
@@ -34,11 +34,11 @@ def test_attn(execute_cpu_dace = False):
     # SM, SN = 32, 32
 
     ##### BASE BERT
-    B = 2
-    H = 12
-    P = 64
-    N = P * H
-    SM, SN = 128, 128
+    # B = 2
+    # H = 12
+    # P = 64
+    # N = P * H
+    # SM, SN = 128, 128
 
     K, Q, V = [
         torch.randn([SM, B, N]),
@@ -46,6 +46,9 @@ def test_attn(execute_cpu_dace = False):
         torch.randn([SM, B, N])
     ]
     ptmodel = torch.nn.MultiheadAttention(N, H, bias=False)
+
+    donnx.ONNXCast.default_implementation = "onnxruntime"
+
 
     pt_outputs = ptmodel(Q, K, V)
 
@@ -72,6 +75,7 @@ def test_attn(execute_cpu_dace = False):
         assert np.allclose(pt_outputs[1].detach().numpy(),
                            dace_outputs_1[1],
                            atol=1e-06)
+    # dace_model.sdfg.from_file('/tmp/out.sdfg')
     sdfg = dace_model.sdfg
     # import pdb
     # pdb.set_trace()
@@ -87,6 +91,8 @@ def test_attn(execute_cpu_dace = False):
 
     sdfg.apply_transformations([FPGATransformSDFG])
     sdfg.expand_library_nodes()
+    sdfg.save('/tmp/out_fpga_pre_inlined.sdfg')
+
     sdfg.apply_transformations_repeated([InlineSDFG])
     sdfg.apply_transformations_repeated(PruneConnectors)
     # sdfg.states()[0].location["is_FPGA_kernel"] = False
@@ -94,7 +100,10 @@ def test_attn(execute_cpu_dace = False):
     sdfg.save('/tmp/out_fpga.sdfg')
 
     # Streaming composition
-    # sdfg.apply_transformations_repeated([InlineSDFG, sm.StreamingComposition], [{}, {"storage": dace.StorageType.FPGA_Local}])
+    sdfg.apply_transformations_repeated([InlineSDFG, sm.StreamingComposition], [{}, {"storage": StorageType.FPGA_Local}], print_report=True)
+    import pdb
+    pdb.set_trace()
+    sdfg.save('/tmp/out_fpga.sdfg')
 
     # Load from file
     # sdfg = SDFG.from_file('/tmp/out_fpga.sdfg')
