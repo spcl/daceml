c6af407ce6b60bf4bdc4109333aebce84d523001
Oliver Rausch <oliverrausch99@gmail.com>
2021-04-07 11:12:11 +0200

    Create testing subpackage

--- tests/autodiff/pytorch/test_training.py
+++ tests/autodiff/pytorch/test_training.py
@@ -9,26 +9,7 @@ from torch import nn, optim
 from transformers import BertLayer, BertConfig
 
 from daceml.pytorch import DaceModule
-
-
-def torch_tensors_close(name, torch_v, dace_v):
-    rtol = 1e-6
-    atol = 1e-4
-    if not torch.allclose(torch_v, dace_v, rtol=rtol, atol=atol):
-        print("torch value: ", torch_v)
-        print("dace value: ", dace_v)
-        print("diff: ", torch.abs(dace_v - torch_v))
-
-        failed_mask = np.abs(torch_v.numpy() - dace_v.numpy()
-                             ) > atol + rtol * np.abs(dace_v.numpy())
-        print(f"wrong elements torch: {torch_v[failed_mask]}")
-        print(f"wrong elements dace: {dace_v[failed_mask]}")
-
-        for x, y in zip(torch_v[failed_mask], dace_v[failed_mask]):
-            print(f"lhs_failed: {abs(x - y)}")
-            print(f"rhs_failed: {atol} + {rtol * abs(y)}")
-
-        assert False, f"{name} was not close)"
+from daceml.testing.utils import torch_tensors_close
 
 
 def training_step(dace_model,
