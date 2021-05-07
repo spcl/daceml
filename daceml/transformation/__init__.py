495c71cfbedba9ec3ba1c2279a47210340336d72
Oliver Rausch <oliverrausch99@gmail.com>
2021-05-07 16:31:02 +0200

    Readd removed EinsumFusion transformation in __init__.py

--- daceml/transformation/__init__.py
+++ daceml/transformation/__init__.py
@@ -1,3 +1,4 @@
 from .constant_folding import ConstantFolding
+from .einsum_fusion import HorizontalEinsumFusion
 from .parameter_to_transient import parameter_to_transient
 from .tasklet_fusion import TaskletFusion
