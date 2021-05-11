2af0bc3cd1881bc4faea0539bd31c5f00457ff90
Tal Ben-Nun <talbn@inf.ethz.ch>
2021-05-11 15:53:22 +0200

    include runtime files in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
 import os
 from setuptools import setup, find_packages
+import glob
+import os
+
+# Find runtime and external library files by obtaining the module path and
+# trimming the absolute path of the resulting files.
+daceml_path = os.path.dirname(os.path.abspath(__file__)) + '/daceml/'
+runtime_files = [
+    f[len(daceml_path):]
+    for f in [glob.glob(daceml_path + '**/*.h', recursive=True) +
+              glob.glob(daceml_path + '**/*.cuh', recursive=True) +
+              glob.glob(daceml_path + '**/*.cu', recursive=True)]
+]
 
 with open("README.md", "r") as fp:
     long_description = fp.read()
@@ -19,10 +31,10 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.6, <3.9',
     packages=find_packages(
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-    package_data={'': ['*.cpp']},
+    package_data={'': ['*.cpp'] + runtime_files},
     install_requires=[
         'dace@git+https://github.com/spcl/dace.git@dml_misc_fixes2',
         'onnx == 1.7.0', 'torch', 'dataclasses; python_version < "3.7"'
