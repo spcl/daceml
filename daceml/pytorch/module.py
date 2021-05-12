4edfe2428dbd5bab98f22ccb94094b2948d96472
Tal Ben-Nun <talbn@inf.ethz.ch>
2021-05-12 08:06:45 +0000

    Another merge fix

--- daceml/pytorch/module.py
+++ daceml/pytorch/module.py
@@ -217,7 +217,7 @@ class DaceModule(nn.Module):
                 for _, hook in self.post_autodiff_hooks.items():
                     hook(function._model.sdfg, function._model.backward_sdfg)
 
-                function._forward_model.compile_and_init()
+                function._model.compile_and_init()
 
                 def forward(*args):
                     args_and_params = list(args)
