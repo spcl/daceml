39ad6df516d5c90554dae5a23cdaa4cf191b4287
Tal Ben-Nun <tbennun@gmail.com>
2021-05-21 23:31:27 +0200

    Add InputToConstant transformation

--- daceml/transformation/__init__.py
+++ daceml/transformation/__init__.py
@@ -1,3 +1,4 @@
 from .constant_folding import ConstantFolding
+from .input_to_constant import InputToConstant
 from .parameter_to_transient import parameter_to_transient
 from .tasklet_fusion import TaskletFusion
