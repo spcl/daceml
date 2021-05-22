a5f53c6266642822eb9d1ed98a8ab2c63d852a8d
Oliver Rausch <oliverrausch99@gmail.com>
2021-05-22 12:26:34 +0200

    Add remove_output_connector function

--- daceml/util/utils.py
+++ daceml/util/utils.py
@@ -1,3 +1,4 @@
+import collections
 import functools
 import logging
 import typing
@@ -199,3 +200,28 @@ def platform_library_name(libname: str) -> str:
     prefix = dace.Config.get('compiler', 'library_prefix')
     suffix = dace.Config.get('compiler', 'library_extension')
     return f"{prefix}{libname}.{suffix}"
+
+
+def remove_output_connector(sdfg: dace.SDFG, state: dace.SDFGState,
+                            node: nd.Node, conn_name: str):
+    """ Remove an output connector (only possible if the connector doesn't write to a non-transient).
+
+        :param sdfg: the sdfg containing the node.
+        :param state: the state containing the node.
+        :param node: the node
+        :param conn_name: the name of the connector to remove
+    """
+    queue = collections.deque(
+        e.dst for e in state.out_edges_by_connector(node, conn_name))
+    while len(queue) > 0:
+        current_node = queue.popleft()
+
+        edges = state.out_edges(current_node)
+        state.remove_node(current_node)
+        for e in edges:
+            if not sdfg.arrays[e.data.data].transient:
+                raise ValueError(
+                    "Tried to remove a connector that wrote to a non-transient"
+                )
+
+            queue.append(e.dst)
