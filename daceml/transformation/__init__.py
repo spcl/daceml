0562a638d6613e39ba4b8f0e779117245c4f4de2
Tal Ben-Nun <tbennun@gmail.com>
2021-05-23 04:23:07 +0200

    Replacement transformation API

--- daceml/transformation/__init__.py
+++ daceml/transformation/__init__.py
@@ -3,3 +3,4 @@ from .input_to_constant import InputToConstant
 from .parameter_to_transient import parameter_to_transient
 from .tasklet_fusion import TaskletFusion
 from .pad_conv_fusion import PadConvFusion
+from .replacement import ReplacementTransformation
