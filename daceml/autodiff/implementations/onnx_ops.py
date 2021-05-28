a438fd10ac871f7f5a601ff3711db9f956e274dc
am-ivanov <am-ivanov@users.noreply.github.com>
2021-05-28 13:48:47 +0200

    Broken PyTorch bwd depthwise conv

--- daceml/autodiff/implementations/onnx_ops.py
+++ daceml/autodiff/implementations/onnx_ops.py
@@ -18,6 +18,7 @@ from daceml.onnx.op_implementations import pure_implementations, \
 import daceml.autodiff.utils as butils
 from daceml.autodiff.base_abc import BackwardImplementation, BackwardContext, BackwardResult
 from daceml.util import utils
+import daceml
 
 
 def reverse_einsum_wrt_input(forward_node: donnx.ONNXEinsum,
@@ -568,6 +569,123 @@ class CuDNNConvBackward(BackwardImplementation):
             code_init=init_code,
             code_exit=finalize_code)
         tasklet.environments = {donnx.environments.cuDNN.full_class_path()}
+
+        nstate.add_edge(
+            nstate.add_read(result.given_grad_names["Y"]), None, tasklet,
+            f"_dY", nsdfg.make_array_memlet((result.given_grad_names["Y"])))
+        for name in required_forward_inputs:
+            nstate.add_edge(nstate.add_read(name), None, tasklet, f"_{name}",
+                            nsdfg.make_array_memlet(name))
+
+        for name in required_gradients:
+            arr_name = result.required_grad_names[name]
+            nstate.add_edge(tasklet, f"_d{name}", nstate.add_write(arr_name),
+                            None, nsdfg.make_array_memlet(arr_name))
+
+        inputs = {result.given_grad_names["Y"]}.union(required_forward_inputs)
+        outputs = {result.required_grad_names[n] for n in required_gradients}
+        node = context.backward_state.add_nested_sdfg(nsdfg, None, inputs,
+                                                      outputs)
+
+        return node, result
+
+
+@autoregister_params(op="Conv", name="PyTorch")
+class PyTorchConvBackward(BackwardImplementation):
+    """ Conv backward using PyTorch.
+    """
+
+    @staticmethod
+    def backward_can_be_applied(node: nd.Node, state: dace.SDFGState,
+                                sdfg: dace.SDFG) -> bool:
+        return True
+
+    @staticmethod
+    def backward(
+            forward_node: nd.Node, context: BackwardContext,
+            given_gradients: List[Optional[str]],
+            required_gradients: List[Optional[str]]
+    ) -> Tuple[nd.Node, BackwardResult]:
+
+        nsdfg = dace.SDFG(forward_node.label + "_backward")
+        X_desc = butils.forward_in_desc_with_name(forward_node, context, "X")
+        W_desc = butils.forward_in_desc_with_name(forward_node, context, "W")
+
+        T = X_desc.dtype
+
+        # setup gradient arrays
+        result = BackwardResult.empty()
+        required_grads = set(required_gradients)
+        for r in required_grads:
+            result.required_grad_names[
+                r] = butils.add_backward_desc_for_connector(nsdfg,
+                                                            forward_node,
+                                                            context,
+                                                            r,
+                                                            input=True)
+        result.given_grad_names["Y"] = butils.add_backward_desc_for_connector(
+            nsdfg, forward_node, context, "Y", input=False)
+
+        # setup non-gradient arrays
+        required_forward_inputs = ["W", "X"]
+        for i in required_forward_inputs:
+            new_desc = copy.deepcopy(
+                butils.forward_in_desc_with_name(forward_node, context, i))
+            new_desc.transient = False
+            nsdfg.add_datadesc(i, new_desc)
+
+        # setup state
+        nstate = nsdfg.add_state()
+        unique_id = "{}_{}_{}_{}_bwd".format(
+            clean_onnx_name(forward_node.name), context.forward_sdfg.sdfg_id,
+            context.forward_sdfg.node_id(context.forward_state),
+            context.forward_state.node_id(forward_node))
+
+        init_code = ""
+        finalize_code = ""
+        code_global = """
+            #include <ATen/Tensor.h>
+            #include <ATen/Functions.h>
+        """
+        tasklet_inputs = {
+            f"_{i}": dace.pointer(T)
+            for i in itertools.chain(["dY"], required_forward_inputs)
+        }
+        tasklet_outputs = {
+            f"_d{i}": dace.pointer(T)
+            for i in itertools.chain(required_gradients)
+        }
+
+        tasklet_code = f"""
+            at::IntArrayRef x_shape = {{ {", ".join(map(str, X_desc.shape))} }};
+            at::IntArrayRef x_strides = {{ {", ".join(map(str, X_desc.strides))} }};
+            at::IntArrayRef w_shape = {{ {", ".join(map(str, W_desc.shape))} }};
+            at::IntArrayRef w_strides = {{ {", ".join(map(str, W_desc.strides))} }};
+            at::Tensor x = at::from_blob(_X, x_shape, x_strides, [](void*){{}}, at::TensorOptions().device(at::kCUDA));
+            at::Tensor w = at::from_blob(_W, w_shape, w_strides, [](void*){{}}, at::TensorOptions().device(at::kCUDA));
+            at::Tensor dy = at::from_blob(_dY, x_shape, x_strides, [](void*){{}}, at::TensorOptions().device(at::kCUDA));
+            at::Tensor dw = at::from_blob(_dW, w_shape, w_strides, [](void*){{}}, at::TensorOptions().device(at::kCUDA));
+            at::Tensor dx = at::from_blob(_dX, x_shape, x_strides, [](void*){{}}, at::TensorOptions().device(at::kCUDA));
+            
+            at::IntArrayRef kernel_shape = {{ {", ".join(map(str, forward_node.kernel_shape))} }};
+            at::IntArrayRef conv_strides = {{ {", ".join(map(str, forward_node.strides))} }};
+            at::IntArrayRef padding = {{ {", ".join(map(str, forward_node.pads))} }};
+            at::IntArrayRef dilation = {{ {", ".join(map(str, forward_node.dilations))} }};
+            std::array<bool, 2> output_mask = {{true, true}};
+            
+            std::tie(dx, dw) = at::thnn_conv_depthwise2d_backward(dy, x, w, kernel_shape, conv_strides, padding, dilation, output_mask);
+        """
+
+        tasklet = nstate.add_tasklet(
+            name=unique_id,
+            inputs=tasklet_inputs,
+            outputs=tasklet_outputs,
+            code=tasklet_code,
+            language=dace.dtypes.Language.CPP,
+            code_global=code_global,
+            code_init=init_code,
+            code_exit=finalize_code)
+        tasklet.environments = {daceml.pytorch.environments.PyTorch.full_class_path()}
 
         nstate.add_edge(
             nstate.add_read(result.given_grad_names["Y"]), None, tasklet,
