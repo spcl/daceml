cb0da618d7af622b423c624833cc6b21ec6153ee
Tiziano De Matteis <tdematt@inf.ethz.ch>
2021-06-10 12:17:52 +0200

    Explicitely disable CUDA for Reshape Elim Test

--- tests/transformation/test_reshape_elimination.py
+++ tests/transformation/test_reshape_elimination.py
@@ -25,7 +25,7 @@ def test_reshape_elimination(sdfg_name):
 
     ptmodel = Model()
     x = torch.rand((100, 6, 12, 12))
-    dace_model = DaceModule(ptmodel, auto_optimize=False, sdfg_name=sdfg_name)
+    dace_model = DaceModule(ptmodel, auto_optimize=False, sdfg_name=sdfg_name, cuda=False)
 
     def ApplyReshapeElimination(dace_module):
         sdfg = dace_module.sdfg
