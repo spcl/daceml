6ada1e9ca92551b9bf3326dfc63bf527bb190156
Oliver Rausch <Oliverrausch99@gmail.com>
2022-01-16 22:35:07 +0100

    Bump dace

--- setup.py
+++ setup.py
@@ -37,8 +37,8 @@ setup(
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     package_data={'': (['*.cpp'] + runtime_files)},
     install_requires=[
-        'dace@git+https://github.com/spcl/dace.git@75c5e22', 'onnx == 1.7.0',
-        'torch', 'dataclasses; python_version < "3.7"'
+        'dace@git+https://github.com/spcl/dace.git@dml-pre-transformation-changes',
+        'onnx == 1.7.0', 'torch', 'dataclasses; python_version < "3.7"'
     ],
     # install with pip and --find-links (see Makefile)
     # See https://github.com/pypa/pip/issues/5898
