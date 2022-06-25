a917ef6b341cd971829b4a9be2d51a97a0cb0c9c
Oliver Rausch <Oliverrausch99@gmail.com>
2022-06-25 18:33:55 +0200

    Add new compute argument to python_pure_op_implementation
    This argument enables computing values based on the node and it's
    connectors. This enables moving a lot of operators to use this
    decorators (TODO in later PR)

--- daceml/onnx/op_implementations/utils.py
+++ daceml/onnx/op_implementations/utils.py
@@ -1,9 +1,8 @@
 import inspect
 import copy
-from typing import Dict, Tuple
+from typing import Dict, Tuple, Optional, Callable, Union, Any
 
 import dace
-import typing
 from dace import SDFGState, SDFG, dtypes, nodes
 from dace.frontend.python.parser import DaceProgram
 from dace.registry import autoregister
@@ -38,8 +37,11 @@ def op_implementation(op, name):
     return dec
 
 
-def program_for_node(program, sdfg: SDFG, state: SDFGState,
-                     node: onnx_op.ONNXOp) -> SDFG:
+def program_for_node(program,
+                     sdfg: SDFG,
+                     state: SDFGState,
+                     node: onnx_op.ONNXOp,
+                     extra_vars: Optional[Dict[str, Any]] = None) -> SDFG:
     """ Expand a function to a dace program.
 
         The dtypes for the arguments will be extracted by matching the parameter names to edges.
@@ -78,6 +80,8 @@ def program_for_node(program, sdfg: SDFG, state: SDFGState,
 
     program.__name__ = node.label + "_expansion"
     result = DaceProgram(program, (), {}, False, dace.DeviceType.CPU)
+    if extra_vars is not None:
+        result.global_vars.update(extra_vars)
 
     sdfg = result.to_sdfg()
 
@@ -125,15 +129,67 @@ def empty_sdfg_for_node(
     return nsdfg, nstate, input_nodes, output_nodes
 
 
-def python_pure_op_implementation(func):
-    """ A decorator that registers an python op implementation. The name of the function will be the name of the op
-        that is being replaced.
+@dace.dtypes.paramdec
+def python_pure_op_implementation(func,
+                                  compute: Optional[Dict[str,
+                                                         Callable]] = None):
+    """
+    A decorator that registers an python op implementation. The name of the
+    function will be the name of the op that is being replaced.
+
+    The compute parameter enables you to compute a variable given the node and
+    it's inputs/outputs. This variable will be namespaced when parsing the function.
+
+    To use this, the names of the functions can be either:
+    * 'node', in which case the argument will be passed the node we are expanding,
+    * or, the name of any connector of the node, in which case the argument will be
+      the data descriptor for that connector
+
+    for example, the following compute argument instantiation will
+    make variables 'axis' and 'shape' available when the function is parsed.
+
+    compute=dict(
+        # grabs the axis of a node
+        axis=lambda node: node.axis
+        # grabs the shape of the connector with name 'data'
+        shape=lambda data: data.shape
+    )
     """
     @op_implementation(op=func.__name__, name="pure")
     class PureImpl(ONNXForward):
         @staticmethod
         def forward(node: onnx_op.ONNXOp, state: SDFGState,
-                    sdfg: SDFG) -> typing.Union[nodes.Node, SDFG]:
-            return program_for_node(func, sdfg, state, node)
+                    sdfg: SDFG) -> Union[nodes.Node, SDFG]:
+            def compute_argument_resolver(arg: str):
+                if arg == "node":
+                    return node
+                elif arg in node.in_connectors:
+                    return in_desc_with_name(node, state, sdfg, arg)
+                elif arg in node.out_connectors:
+                    return out_desc_with_name(node, state, sdfg, arg)
+                else:
+                    raise ValueError(
+                        "Got unknown compute argument {}."
+                        " Arguments to compute can be either 'node',"
+                        " or the name of a connector of the node".format(arg))
+
+            extra_vars = {}
+            if compute is not None:
+                for var_name, function in compute.items():
+
+                    # get the names of the lambda
+                    argument_names = list(
+                        inspect.signature(function).parameters)
+
+                    args = map(compute_argument_resolver, argument_names)
+                    var_value = function(*args)
+
+                    extra_vars[var_name] = var_value
+
+            return program_for_node(func,
+                                    sdfg,
+                                    state,
+                                    node,
+                                    extra_vars=extra_vars)
 
     return PureImpl
