Fix softmax, downgrade onnx
Created using spr 1.3.4-beta.1

daceml/onnx/op_implementations/pure_implementations.py
@@ -310,7 +310,7 @@ softmax_compute = dict(
     keepdims=lambda node: node.axis != 0)
 
 
-@python_pure_op_implementation(compute=softmax_compute)
+@python_pure_op_implementation(**softmax_compute)
 def Softmax(input, output):
     maximum = np.maximum.reduce(input, axis=axis, keepdims=keepdims)
     exponent = np.exp(input - maximum)