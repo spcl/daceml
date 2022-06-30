c953e98041641ffd95d08820231393737c55f2f7
Oliver Rausch <Oliverrausch99@gmail.com>
2022-07-01 00:59:34 +0200

    Bump dace to 0.13.3
    Created using spr 1.3.4-beta.1

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     package_data={'': (['*.cpp'] + runtime_files)},
     install_requires=[
-        'dace@git+https://github.com/spcl/dace.git@40d735',
+        'dace == 0.13.3',
         'onnx == 1.8.0',  # we support opset v12
         'torch',
         'protobuf == 3.19',
